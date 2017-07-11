2bdf4a4d89aadfd5e16fb0666596573d9c33c766
Mauricio Escobar <snkpit@gmail.com>
2017-07-11 18:56:33 -0300

    Arquivos iniciais.

---
+++ app/src/main/java/br/edu/ifsul/charqueadas/oficinadejogos/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsul.charqueadas.oficinadejogos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+   }
+}
